e991a0a621c5f1f4fb12d62a9defb31d0f53a4a4
julie-manee <133013256+julie-manee@users.noreply.github.com>
2025-01-14 16:48:01 +0100

    Update Board.java

--- Khet/src/main/Board.java
+++ Khet/src/main/Board.java
@@ -13,25 +13,6 @@ public class Board {
 	public static final int SQUARE_SIZE = 64;
 	private BufferedImage boardImage;
 	
-	/**
-	//public static final int HALF_SQUARE_SIZE = SQUARE_SIZE/2;
-	
-	public void draw(Graphics2D g2) {
-        // Définir la couleur de fond
-        g2.setColor(new Color(128, 128, 128));  // Gris
-        g2.fillRect(0, 0, MAX_COL * SQUARE_SIZE, MAX_ROW * SQUARE_SIZE);
-
-        // Dessiner les cases et la grille
-        g2.setColor(Color.BLACK);  // Couleur de la grille
-
-        for (int row = 0; row < MAX_ROW; row++) {
-            for (int col = 0; col < MAX_COL; col++) {
-                // Dessiner une case vide
-                g2.drawRect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-            }
-        }
-    }**/
-	
 	// Constructeur pour charger l'image du plateau
     public Board() {
         try {
