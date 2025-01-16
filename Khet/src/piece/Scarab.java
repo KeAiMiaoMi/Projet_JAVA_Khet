4d28da9fc48d90ed52e2e9ae65fad04fbd57771e
julie-manee <133013256+julie-manee@users.noreply.github.com>
2025-01-17 00:03:39 +0100

    Update Scarab.java isSwitchPossible() added

--- Khet/src/piece/Scarab.java
+++ Khet/src/piece/Scarab.java
@@ -15,4 +15,45 @@ public class Scarab extends Piece{
 		
 	}
 	
+	public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard(targetCol, targetRow)) {
+			if(Math.abs(targetCol - preCol) + Math.abs(targetRow - preRow) == 1 ||
+					Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 1) { //col:row ratio needs to be 1:1
+				if(isValidSquare(targetCol, targetRow)) {
+					return true;
+				}
+			}
+			
+			//Switch
+			if(isSwitchPossible(targetCol, targetRow)) {   
+				for(Piece piece : GamePanel.simPieces) {
+					if(piece.col == targetCol && piece.row == targetRow){
+						GamePanel.switchingP = piece;
+						return true;
+					}
+				}
+			}
+			
+		}
+		
+		return false;
+		
+	}
+	
+	private boolean isSwitchPossible(int targetCol, int targetRow) {
+	    if (Math.abs(targetCol - this.col) <= 1 && Math.abs(targetRow - this.row) <= 1 && 
+	    		(Math.abs(targetCol - preCol) + Math.abs(targetRow - preRow) == 1 ||
+					Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 1)) {
+	        // Trouver la pièce sur la case cible
+	        for (Piece piece : GamePanel.simPieces) {
+	            if (piece.col == targetCol && piece.row == targetRow) {
+	                if (!(piece instanceof Scarab) && !(piece instanceof Pharoah) && piece.color == this.color) {
+	                    return true;
+	                }
+	            }
+	        }
+	    }
+	    return false;
+	}
 }
