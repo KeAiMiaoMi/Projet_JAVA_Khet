8dd2d429b98329b0153f11f4237bb6f3b9e8f645
julie-manee <133013256+julie-manee@users.noreply.github.com>
2025-01-16 10:54:19 +0100

    Update Pharaoh.java

--- Khet/src/piece/Pharaoh.java
+++ Khet/src/piece/Pharaoh.java
@@ -2,14 +2,14 @@ package piece;
 
 import main.GamePanel;
 
-public class Pharaoh extends Piece{
-	public Pharaoh(int color, int col, int row) {
+public class Pharoah extends Piece{
+	public Pharoah(int color, int col, int row) {
 		super(color, col, row);
 		
 		if(color == GamePanel.WHITE) {
-			image = getImage("/piece/white_Pharaoh_0");
+			image = getImage("/piece/white_Pharoah_0");
 		}else {
-			image = getImage("/piece/red_Pharaoh_0");
+			image = getImage("/piece/red_Pharoah_0");
 		}
 		
 	}
