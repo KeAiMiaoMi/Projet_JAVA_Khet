d27445f727a77c284aa6a86c565be9890df44dfe
Elie Lin <138505255+KeAiMiaoMi@users.noreply.github.com>
2025-01-17 05:45:42 +0100

    Add files via upload

---
+++ Khet_complet/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame("Khet");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		
+		//Add GamePanel to window
+		GamePanel gp = new GamePanel();
+		window.add(gp);
+		window.pack();
+		
+		
+		//window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gp.launchGame();
+		
+	}
+
+}
