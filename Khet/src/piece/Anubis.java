416a1e3d3dbd9735cd3110ffffd55d8f373b7023
julie-manee <133013256+julie-manee@users.noreply.github.com>
2025-01-17 01:14:38 +0100

    Update Anubis.java canMove() added

--- Khet/src/piece/Anubis.java
+++ Khet/src/piece/Anubis.java
@@ -12,8 +12,21 @@ public class Anubis extends Piece {
 		}else {
 			image = getImage("/piece/red_Anubis_0");
 		}
-		
 	}
 	
-	
+	public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard(targetCol, targetRow)) {
+			if(Math.abs(targetCol - preCol) + Math.abs(targetRow - preRow) == 1 ||
+					Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 1) { //col:row ratio needs to be 1:1
+				if(isValidSquare(targetCol, targetRow)) {
+					if(isTileSameColor(targetCol, targetRow)) {
+						return true;
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
 }
