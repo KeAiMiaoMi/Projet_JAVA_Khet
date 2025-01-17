6ae56da7f715d3402759116cb898b43ea7d7e814
julie-manee <133013256+julie-manee@users.noreply.github.com>
2025-01-17 15:48:34 +0100

    Add files via upload

---
+++ Khet_final/src/piece/Piece.java
@@ -0,0 +1,163 @@
+package piece;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.Board;
+import main.GamePanel;
+
+public class Piece {
+	
+	public BufferedImage image;
+	public int x, y;
+	public int col, row, preCol, preRow;
+	public int color, orientation;
+	public Piece hittingP;
+	
+	public Piece(int color, int col, int row, int orientation) {
+		this.color = color;
+		this.col = col;
+		this.row = row;
+		this.orientation = orientation;
+		x = getX(col);
+		y = getY(row);
+		preCol = col;
+		preRow = row;
+	}
+	
+	public BufferedImage getImage(String imagePath) {
+		BufferedImage image  = null;
+		
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
+	
+	public int getX(int col) {
+		return col * Board.SQUARE_SIZE;
+	}
+	public int getY(int row) {
+		return row * Board.SQUARE_SIZE;
+	}
+	//Center  of the piece
+	public int getCol(int x) {
+		return (x + Board.HALF_SQUARE_SIZE)/Board.SQUARE_SIZE;
+	}
+	
+	public int getRow(int y) {
+		return (y + Board.HALF_SQUARE_SIZE)/Board.SQUARE_SIZE;
+	}
+	
+	public void updatePosition() {
+		x = getX(col);
+		y = getY(row);
+		preCol = getCol(x);
+		preRow = getRow(y);
+	}
+	
+	public void resetPosition() {
+		col = preCol;
+		row = preRow;
+		x = getX(col);
+		y = getY(row);
+	}
+	
+	public boolean canMove(int targetCol, int targetRow) {
+		return false;
+	}
+	
+	public boolean isWithinBoard(int targetCol, int targetRow) {
+		if(targetCol >= 1 && targetCol <= 10 && targetRow >= 1 && targetRow <= 8) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isTileSameColor(int targetCol, int targetRow) {
+		if(this.color == GamePanel.RED) {
+			if (targetCol != 1 && !(targetCol == 9 && (targetRow == 1 || targetRow == 8))) {
+				return true;
+			}
+		}else if(this.color == GamePanel.WHITE) {
+			if (targetCol != 10 && !(targetCol == 2 && (targetRow == 1 || targetRow == 8))) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public Piece getHittingP(int targetCol, int targetRow) {
+		for(Piece piece : GamePanel.simPieces) {
+			if(piece.col == targetCol && piece.row == targetRow && piece != this) {
+				return piece;
+			}
+		}
+		return null;
+	}
+	
+	public boolean isValidSquare(int targetCol, int targetRow) {
+		hittingP = getHittingP(targetCol, targetRow);
+		
+		if(hittingP == null) { //square vacant
+			return true;
+		}else { //square occupied
+			hittingP = null;
+		}
+		
+		return false;
+	}
+	
+	public void draw(Graphics2D g2) {
+		g2.drawImage(image, x, y, Board.SQUARE_SIZE, Board.SQUARE_SIZE, null);
+	}
+	
+	
+	// Getters
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+	public GamePanel getParentPanel() {
+    	return (GamePanel) this.getParentPanel();
+	}
+
+	public int getOrientation() {
+		return orientation;
+	}
+
+	// Modifier l'orientation
+	public void setOrientation(int orientation){
+		this.orientation = orientation %360;
+	}
+
+	// Réaction au laser
+	public void reactLaser(int dx, int dy){
+		return;
+	}
+
+	// Rotation des pièces
+	public void rotate(boolean isRightTurn) {
+        if (isRightTurn) {
+            orientation = (orientation + 90) % 360;
+        } else {
+            orientation = (orientation + 270) % 360;
+    	}
+		updateImage();
+	}
+
+	public void updateImage(){
+		return;
+	}
+	
+}
